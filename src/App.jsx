1e2dbb21cbafae5bed34c0a4277ff24df0a69405
Rachana Hegde <hegde.rachana99@gmail.com>
2025-06-15 19:46:37 +0100

    Update Navbar and Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./index.css";
 // Import pages
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Home />
     </>
   );
