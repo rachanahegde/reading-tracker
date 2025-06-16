7b1189ddd20917bd7b1a907603acf7e3b61150bc
Rachana Hegde <hegde.rachana99@gmail.com>
2025-06-16 13:38:38 +0100

    Update home and create MainCreatureCard component

---
+++ src/components/MainCreatureCard.jsx
@@ -0,0 +1,5 @@
+export default function MainCreatureCard() {
+  return <></>;
+}
+
+// TODO Build card displaying dragon with background, buttons for adding book and selecting background, creature progress bar, books read, total XP, menagerie button and view all books btn
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
+import MainCreatureCard from "../components/MainCreatureCard";
+
 export default function Home() {
   return (
-    <>
-      <h1> </h1>
-    </>
+    <div className="ml-6">
+      <h1 className="font-josefin text-dark-brown italic text-xl mt-4 text-center">
+        Grow your dragon by reading books.
+      </h1>
+      {/* Card displaying dragon with background, buttons for adding book and selecting background, creature progress bar, books read, total XP, menagerie button and view all books btn */}
+      <MainCreatureCard />
+    </div>
   );
 }
-
-// TODO Set up navbar
-// TODO Set up custom theme colors and fonts
 
 // TODO Set up context API with local storage for tracking book data
 // TODO Track evolution of the assigned creature and XP required to evolve it
